baba38945b98b521dca150683591108614f91f66
Sazzadur Rahman <sazzadur.rahman@gmail.com>
2015-05-10 15:01:49 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-## Getting full dataset
+## Getting full dataset. The "household_power_consumption.txt" file is in the current working directory.
 data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
